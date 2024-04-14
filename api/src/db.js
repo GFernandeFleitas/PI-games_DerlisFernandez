718deb222a8369e9057e94a71d0a1fe47d6d0d53
GFernandeFleitas <derlis.fernandez15@gmail.com>
2024-04-14 14:26:24 -0400

    database change

--- api/src/db.js
+++ api/src/db.js
@@ -6,13 +6,26 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 console.log(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`);
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`,
+//   {
+//     logging: false, // set to console.log to see the raw SQL queries
+//     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//   }
+// );
+
+const sequelize = new Sequelize(DB_DEPLOY, {
+  dialect: "postgres",
+  logging: false,
+  native: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Usar false si no tienes un certificado de CA válido
+    },
+  },
+});
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
